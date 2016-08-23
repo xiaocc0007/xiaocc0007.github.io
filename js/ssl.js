1d809f3733f1ea140c56f1598c59551c2fa07962
xiaocc0007 <574167872@qq.com>
2016-08-23 22:50:24 +0800

    bigboss

---
+++ js/ssl.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    /*页面强转开始*/
+    var tmpTag = 'https:' == document.location.protocol ? false : true;
+    if(tmpTag){
+        window.location='https://htmlpreview.github.io/?https://github.com/xiaocc0007/3d/blob/master/demo.html';
+    }
+    /*页面强转结束*/
+};
